411ad57474c92cf10672ad443e0652b74dcd7b53
bpshaver <benpshaver@gmail.com>
2017-11-28 16:16:29 -0500

    Fixing empty submission script

--- 03_Kaggle_Submission.py
+++ 03_Kaggle_Submission.py
@@ -5,3 +5,14 @@ Created on Thu Nov 16 19:38:57 2017
 @author: benps
 """
 
+import pandas as pd
+np.random.seed(6)
+#%%
+
+predictions = pd.read_csv('Predictions.csv', index_col=0).rename(columns={'1':'WnvPresent'})
+
+predictions['Id'] = range(1,predictions.shape[0]+ 1)
+
+submission = predictions[['Id','WnvPresent']]
+
+submission.to_csv('submission.csv', index=False)
